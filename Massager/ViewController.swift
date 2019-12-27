e981a18ecfc6e2dc23be6564126a457c60eae32d
Roman Danyliuk <rokimjour@gmail.com>
2019-12-27 22:30:03 +0200

    Starter project

---
+++ Massager/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Massager
+//
+//  Created by Romaha  on 27.12.2019.
+//  Copyright © 2019 Romaha . All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
