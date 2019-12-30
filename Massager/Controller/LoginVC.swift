808ca239f93d679b9415b809e7b600254ce0ec70
Roman Danyliuk <rokimjour@gmail.com>
2019-12-30 17:41:55 +0200

    create Login VC

---
+++ Massager/Controller/LoginVC.swift
@@ -0,0 +1,22 @@
+//
+//  LoginVC.swift
+//  Massager
+//
+//  Created by Romaha  on 30.12.2019.
+//  Copyright © 2019 Romaha . All rights reserved.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    
+    }
+
+    @IBAction func closePressed(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+}
---
+++ Massager/Utilities/Constans.swift
@@ -0,0 +1,13 @@
+//
+//  Constans.swift
+//  Massager
+//
+//  Created by Romaha  on 30.12.2019.
+//  Copyright © 2019 Romaha . All rights reserved.
+//
+
+import Foundation
+
+//Segues
+
+let TO_LOGIN = "toLogin"
